ce93ba16e39834f1b58dd0d510b47b1d85015a82
harish-datla <36021808+harish-datla@users.noreply.github.com>
2022-08-26 19:42:35 +0530

    Create site.php

---
+++ FreeCodeCamp/L1/site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo("Hello World");
+      echo "<h1>Harish's Site</h1>";
+      echo "<hr>";
+      echo "<p> This is my site </p>"      
+     ?>
+  </body>
+</html>
