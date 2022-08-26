6855b3f5352119812966215b68c2159e4b2c35d2
harish-datla <36021808+harish-datla@users.noreply.github.com>
2022-08-27 01:35:33 +0530

    Create site.php

---
+++ FreeCodeCamp/L8/site.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php
+      //Arrays
+        $friends = array("kevin", "karen","oscar","jim");
+        echo count($friends);
+        $friends[1] = 5;
+        $friends[4] = "bro";
+        $friends[10] = "gym";
+       ?>
+  </body>
+</html>
