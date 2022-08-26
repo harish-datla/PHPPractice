502d5368a17883da59a4b4746734d4f3bb312c09
harish-datla <36021808+harish-datla@users.noreply.github.com>
2022-08-26 22:38:42 +0530

    Create site.php

---
+++ FreeCodeCamp/L3/site.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php 
+      // Variables
+      $phrase = "To be or not to be"
+      $age = 30;
+      $gpa = 3.7;
+      $isMale = false;
+
+      echo "Hello";
+      echo 4.57;
+      echo $phrase;
+
+      
+     ?>
+      
+     ?>
+  </body>
+</html>
