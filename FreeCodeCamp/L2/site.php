8bf777499cdd811a24c78a5d686d2c135aa19962
harish-datla <36021808+harish-datla@users.noreply.github.com>
2022-08-26 19:43:15 +0530

    Create site.php

---
+++ FreeCodeCamp/L2/site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+      $characterName = "Mobis";
+      $characterAge = 28;
+
+      echo "There once was a man named $characterName <br>";
+      echo "He was $characterAge years old <br>";
+      echo "He really liked the name $characterName <br>";
+      echo "But didn't like being $characterAge <br>";
+      
+     ?>
+  </body>
+</html>
